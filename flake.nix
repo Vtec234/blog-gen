18c1ac070bd60f0072730f00f7ee0bab45a1c221
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2024-01-31 13:42:57 -0500

    Haskell has by a significant margin the worst build system(s) of any programming language to date

--- flake.nix
+++ flake.nix
@@ -3,51 +3,43 @@
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
 
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      supportedSystems = [
-        # https://github.com/purenix-org/purenix/issues/34#issuecomment-981290387
-        #"x86_64-linux"
-        #"x86_64-darwin"
-        #"aarch64-linux"
-        "aarch64-darwin"
-      ];
-    in
-      flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        overlays = [ haskellNix.overlay
+      overlays = [
+        haskellNix.overlay
           (final: prev: {
             blogProject =
-              final.haskell-nix.project' {
+              final.haskell-nix.cabalProject' {
                 src = ./.;
                 compiler-nix-name = "ghc964";
-                # Tools to provide in `nix develop .`
-                shell.tools = {
-                  cabal = {};
-                  hlint = {};
-                  haskell-language-server = {};
+                # Configuration of `nix develop .` and the direnv shell
+                shell = {
+                  tools = {
+                    cabal = {};
+                    hlint = {};
+                    haskell-language-server = {};
+                  };
+                  buildInputs = with pkgs; [
+                    nodejs
+                    python311Packages.pygments
+                  ];
+                  withHoogle = false;
+                  exactDeps = true;
                 };
-                shell.buildInputs = with pkgs; [
-                  nodejs
-                  python311Packages.pygments
-                ];
-                supportHpack = true;
               };
           })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.blogProject.flake {};
-      in flake // {
-        legacyPackages = pkgs;
-        # Built by `nix build .`
-        packages.default = flake.packages."blog-gen:exe:site";
-      });
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.blogProject.flake {};
+    in flake // {
+      legacyPackages = pkgs;
+      # Default target for `nix build .`
+      packages.default = flake.packages."blog-gen:exe:site";
+    });
 
-  # --- Flake Local Nix Configuration ----------------------------
+  # Local options for `nix`
   nixConfig = {
-    # This sets the flake to use the IOG nix cache.
-    # Nix should ask for permission before using it,
-    # but remove it here if you do not want it to.
-    extra-substituters = ["https://cache.iog.io"];
+    extra-substituters = [ "https://cache.iog.io" ];
     allow-import-from-derivation = "true";
   };
 }
